14f97a34a79c70a6f3b974f5537c5aee4d17c0ac
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-18 08:11:59 +0545

    added function to find the meaning of word 'w'

--- dictionary.py
+++ dictionary.py
@@ -4,3 +4,13 @@ This contains all the code required to read word meanings from data file to matc
 import json
 
 data = json.load(open("data.json"))
+
+def meaning(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist."
+
+word = input("Enter the Word: ")
+print(meaning(word))
