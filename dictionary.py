a6e63a41edf9ce4a3b7eda5cf59b14f10df60fb1
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-18 08:42:48 +0545

    added get_close_matches library function to find closest match of misspelled word

--- dictionary.py
+++ dictionary.py
@@ -11,6 +11,14 @@ def meaning(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w,data.keys())) > 0:
+        yn = input("Did you mean %s instead? Yes or No? " % get_close_matches(w,data.keys())[0])
+        if yn == 'y':
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn =='n':
+            return "You should have entered either y or n"
+        else:
+            return "The word doesnt exist."
     else:
         return "The word doesn't exist."
 
